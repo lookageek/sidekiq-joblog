929964849c11d56daf7f752b92a434a873866777
Jayanth Manklu <jayanthms7@gmail.com>
2018-09-25 11:45:30 +0530

    Use integer millis rather than float millis

--- lib/sidekiq/middleware/server/joblog.rb
+++ lib/sidekiq/middleware/server/joblog.rb
@@ -4,9 +4,9 @@ module Sidekiq
   module Middleware
     module Server
       class JobLog
-        def initialize(model:, application:, except: [])
+        def initialize(model:, app: '', except: [])
           @model         = model
-          @application   = application
+          @app           = app
           @excluded_jobs = Set.new(except)
         end
 
@@ -16,15 +16,17 @@ module Sidekiq
             return
           end
 
-          start_time = Time.now.to_f
+          start_time = current_time_millis
+          queued_at  = get_millis(msg['created_at'])
 
           data = {
-            job_klass: worker.class.name,
-            jid: msg['jid'],
+            job_class: worker.class.name,
+            app: @app,
+            job_id: msg['jid'],
             queue: queue,
             event: 'start',
-            queued_at: msg['created_at'],
-            waited: start_time - msg['created_at']
+            queued_at: queued_at,
+            waited: start_time - queued_at
           }
           save(data)
 
@@ -36,9 +38,9 @@ module Sidekiq
             data[:error] = e.class.name
           end
 
-          end_time = Time.now.to_f
+          end_time = current_time_millis
           data[:worked] = end_time - start_time
-          data[:total]  = end_time - msg['created_at']
+          data[:total]  = end_time - queued_at
           save(data)
 
           raise e if e
@@ -48,6 +50,14 @@ module Sidekiq
 
         def save(data)
           @model.create(data) rescue nil
+        end
+
+        def current_time_millis
+          get_millis(Time.now.to_f)
+        end
+
+        def get_millis(float_time)
+          (float_time.round(3) * 1000).to_i
         end
       end
     end
